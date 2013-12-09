5df28f60f6d93a56290a293bfa6f05ab647d48ad
emu_wahwah <waller_matthew@yahoo.co.uk>
2013-12-09 21:10:25 +0000

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -12,7 +12,7 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 from google.appengine.api import memcache
 
-SECRET = 'ihopethisbloodyworks'
+SECRET = 'XXXXXXXXXXXXX' # insert own secret phrase here
 KEY    = 'top'
 
 def hash_str(s):
